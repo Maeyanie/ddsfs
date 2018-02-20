60c20faa2a16742f4eb0a7efc6b038f2706da031
Maeyanie <me@maeyanie.com>
2018-02-20 18:19:32 -0500

    Add options for memory-only (-o nocache) and RGB (-o rgb)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-xpfs: xpfs.cpp
-	$(CXX) -Wall -Wno-unused-variable -O3 -msse -msse2 -flto -o xpfs xpfs.cpp halveimage.cpp \
+xpfs: xpfs.cpp halveimage.cpp convert.cpp
+	$(CXX) -Wall -Wno-unused-variable -O3 -msse -msse2 -flto -o xpfs xpfs.cpp halveimage.cpp convert.cpp \
 		-IFastDXT FastDXT/util.cpp FastDXT/dxt.cpp FastDXT/intrinsic.cpp \
 		-D_FILE_OFFSET_BITS=64 `pkg-config fuse libturbojpeg --cflags --libs`
--- xpfs.cpp
+++ xpfs.cpp
@@ -25,177 +25,54 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <sys/time.h>
-#include <sys/timeb.h> 
+#include <sys/time.h> 
 #include <dirent.h>
 #include <errno.h>
+#include <pthread.h>
 #include <fuse.h>
-#include <turbojpeg.h>
-#include <libdxt.h>
+#include <atomic>
+#include <unordered_map>
+#include "xpfs.h"
+using namespace std;
 
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
-struct DDS_PIXELFORMAT {
-	unsigned int dwSize;
-	unsigned int dwFlags;
-	unsigned int dwFourCC;
-	unsigned int dwRGBBitCount;
-	unsigned int dwRBitMask;
-	unsigned int dwGBitMask;
-	unsigned int dwBBitMask;
-	unsigned int dwABitMask;
+struct CacheEntry {
+	unsigned char* data;
+	unsigned int len;
 };
-struct ddsheader {
-	unsigned int dwMagic;
-	unsigned int dwSize;
-	unsigned int dwFlags;
-	unsigned int dwHeight;
-	unsigned int dwWidth;
-	unsigned int dwPitchOrLinearSize;
-	unsigned int dwDepth;
-	unsigned int dwMipMapCount;
-	unsigned int dwReserved1[11];
-	DDS_PIXELFORMAT ddspf;
-	unsigned int dwCaps;
-	unsigned int dwCaps2;
-	unsigned int dwCaps3;
-	unsigned int dwCaps4;
-	unsigned int dwReserved2;
-};
-void halveimage(const unsigned char* src, int width, int height, unsigned char* dst);
 
-static char* basepath;
+struct Config {
+	char* basepath;
+	int cache;
+	int compress;
+} config;
+#define XPFS_OPT(t, p, v) { t, offsetof(struct Config, p), v }
+static struct fuse_opt xpfs_opts[] = {
+	XPFS_OPT("dxt1",		compress, 1),
+	XPFS_OPT("rgb",			compress, 0),
+	XPFS_OPT("cache",		cache, 1),
+	XPFS_OPT("nocache",		cache, 0),
+	FUSE_OPT_END
+};
+
+static unordered_map<int,CacheEntry> memcache;
+static int nextfd = 100;
+static pthread_mutex_t cachelock = PTHREAD_MUTEX_INITIALIZER;
 
 static int xpfs_opt_proc(void *data, const char *arg, int key, struct fuse_args *outargs) {
-	if (key == FUSE_OPT_KEY_NONOPT && basepath == NULL) {
-		basepath = strdup(arg);
+	if (key == FUSE_OPT_KEY_NONOPT && config.basepath == NULL) {
+		config.basepath = strdup(arg);
 		return 0;
 	}
 	return 1;
 }
 
-static void xpfs_dds(char* src, char* dst) {
-	struct timeb start, mid, end;
-	
-	if (DEBUG) {
-		printf("turbojpeg: Doing intenal conversion with turbojpeg.\n");
-		ftime(&start);
-	}
-	
-	int fd = open(src, O_RDONLY);
-	if (fd == -1) {
-		fprintf(stderr, "turbojpeg: Could not open '%s' for read.\n", src);
-		return;
-	}
-	
-	size_t size = lseek(fd, 0, SEEK_END);
-	unsigned char* jpeg = (unsigned char*)memalign(16, size);
-	lseek(fd, 0, SEEK_SET);
-	size = read(fd, jpeg, size);
-	close(fd);
-
-	tjhandle tj = tjInitDecompress();
-	
-	int width, height, subsamp, colourspace;
-	if (tjDecompressHeader3(tj, jpeg, size, &width, &height, &subsamp, &colourspace) == -1) {
-		fprintf(stderr, "turbojpeg: Could not decode header for '%s': %s\n", src, tjGetErrorStr());
-		free(jpeg);
-		return;
-	}
-	
-	unsigned char* rgba = (unsigned char*)memalign(16, width * height * 4);
-	if (tjDecompress2(tj, jpeg, size, rgba, 0, 0, 0, TJPF_RGBA, TJFLAG_FASTDCT|TJFLAG_FASTUPSAMPLE) == -1) {
-		fprintf(stderr, "turbojpeg: Could not decode image for '%s': %s\n", src, tjGetErrorStr());
-		free(jpeg);
-		free(rgba);
-		return;
-	}
-	free(jpeg);
-	
-	tjDestroy(tj);
-	
-	if (DEBUG) {
-		ftime(&mid);
-		int diff = (1000.0 * (mid.time - start.time) + (mid.millitm - start.millitm));
-		printf("turbojpeg: JPEG decode done in %d ms.\n", diff);
-	}
-
-	unsigned char* dds = (unsigned char*)memalign(16, width * height * 4);
-	ddsheader header;
-	memset(&header, 0, sizeof(header));
-	header.dwMagic = 0x20534444;
-	header.dwSize = 124;
-	header.dwFlags = 0x1 | 0x2 | 0x4 | 0x1000 | 0x20000;
-	header.dwHeight = height;
-	header.dwWidth = width;
-	
-	int mips = 0;
-	while ((height >> mips) > 8 && (width >> mips) > 8) mips++;
-	if (DEBUG >= 2) printf("turbojpeg: Writing header for %d mips.\n", mips+1);
-	header.dwMipMapCount = mips+1;
-	
-	DDS_PIXELFORMAT ddspix;
-	memset(&ddspix, 0, sizeof(ddspix));
-	ddspix.dwSize = 32;
-	ddspix.dwFlags = 0x4;
-	ddspix.dwFourCC = 'D' | 'X'<<8 | 'T'<<16 | '1'<<24;
-	header.ddspf = ddspix;
-	
-	header.dwCaps = 0x1000 | 0x8 | 0x400000;
-	
-	fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1) {
-		fprintf(stderr, "turbojpeg: Could not open '%s' for write.\n", dst);
-		free(dds);
-		return;
-	}
-	write(fd, &header, sizeof(header));
-
-	int bytes;
-	CompressImageDXT1(rgba, dds, width, height, bytes);
-	if (bytes != width * height / 2) printf("turbojpeg: DXT data size %d, expected %d.\n", bytes, width * height / 2);
-
-	write(fd, dds, bytes);
-
-	int curmip = 0;
-	while (width > 8 && height > 8) {
-		if (DEBUG >= 2) printf("turbojpeg: Resample mip %d (%d x %d)\n", ++curmip, width, height);
-		unsigned char* nextmip = (unsigned char*)memalign(16, width * height * 4);
-		halveimage(rgba, width, height, nextmip);
-		width >>= 1;
-		height >>= 1;
-		free(rgba);
-		rgba = nextmip;
-		
-		if (DEBUG >= 2) printf("turbojpeg: Compress mip %d (%d x %d)\n", curmip, width, height);
-		CompressImageDXT1(rgba, dds, width, height, bytes);
-		if (bytes != width * height / 2) printf("turbojpeg: DXT data size %d, expected %d.\n", bytes, width * height / 2);
-		
-		write(fd, dds, bytes);
-		if (DEBUG >= 2) printf("turbojpeg: Done mip %d.\n", curmip);
-	}
-	close(fd);
-	free(rgba);
-	free(dds);
-	
-	if (DEBUG) {
-		ftime(&end);
-		int diff = (1000.0 * (end.time - mid.time) + (end.millitm - mid.millitm));
-		printf("turbojpeg: DXT1 encode done in %d ms.\n", diff);
-		diff = (1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
-		printf("turbojpeg: Total time %d ms.\n", diff);
-	}
-}
-
 static int xpfs_getattr(const char *path, struct stat *stbuf)
 {
 	int res;
-	char rwpath[strlen(basepath)+strlen(path)+2];
+	char rwpath[strlen(config.basepath)+strlen(path)+2];
 	char* ext;
 
-	sprintf(rwpath, "%s/%s", basepath, path);
+	sprintf(rwpath, "%s/%s", config.basepath, path);
 	if (DEBUG >= 2) printf("getattr: %s\n", rwpath);
 
 	res = lstat(rwpath, stbuf);
@@ -217,13 +94,13 @@ static int xpfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 {
 	DIR *dp;
 	struct dirent *de;
-	char rwpath[strlen(basepath)+strlen(path)+2];
+	char rwpath[strlen(config.basepath)+strlen(path)+2];
 	char* ext;
 
 	(void) offset;
 	(void) fi;
 	
-	sprintf(rwpath, "%s/%s", basepath, path);
+	sprintf(rwpath, "%s/%s", config.basepath, path);
 	
 	if (DEBUG) printf("readdir: %s\n", rwpath);
 	dp = opendir(rwpath);
@@ -265,11 +142,11 @@ static int xpfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 static int xpfs_open(const char *path, struct fuse_file_info *fi)
 {
 	int res;
-	char rwpath[strlen(basepath)+strlen(path)+2];
+	char rwpath[strlen(config.basepath)+strlen(path)+2];
 	char* ext;
 	struct stat st;
 	
-	sprintf(rwpath, "%s/%s", basepath, path);
+	sprintf(rwpath, "%s/%s", config.basepath, path);
 	
 	if (DEBUG) printf("open: %s\n", rwpath);
 	res = open(rwpath, fi->flags);
@@ -282,13 +159,46 @@ static int xpfs_open(const char *path, struct fuse_file_info *fi)
 			ext = strrchr(srcpath, '.');
 			strcpy(ext, ".jpg");
 			
+			
 			res = stat(srcpath, &st);
 			if (res) return -errno;
 			
-			xpfs_dds(srcpath, rwpath);	
+			unsigned char* dds;
+			int len;
+			if (config.compress) len = xpfs_dds_dxt1(srcpath, &dds);
+			else len = xpfs_dds_rgb(srcpath, &dds);
+			if (len == -1) return -errno;
 			
-			res = open(rwpath, fi->flags);
-			if (res == -1) return -errno;
+			if (config.cache) {
+				if (DEBUG >= 2) printf("cache: Writing %d bytes to '%s'\n", len, rwpath);
+				int fd = open(rwpath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (fd == -1) {
+					fprintf(stderr, "cache: Could not open '%s' for write.\n", rwpath);
+					free(dds);
+					return -errno;
+				}
+				len = write(fd, dds, len);
+				close(fd);
+				if (DEBUG >= 2) printf("cache: Wrote %d bytes.\n", len);
+				
+				res = open(rwpath, fi->flags);
+				if (DEBUG >= 2) printf("cache: Reopen returned %d.\n", res);
+				if (res == -1) return -errno;
+				fi->fh = res;
+				return res;
+			} else {
+				pthread_mutex_lock(&cachelock);
+				int fd;
+				do {
+					fd = nextfd++;
+					if (nextfd > 1000000) nextfd = 100;
+				} while (memcache.find(fd) != memcache.end());
+				if (DEBUG >= 2) printf("nocache: Using FD %d for %d bytes.\n", fd, len);
+				memcache[fd] = {dds, (unsigned int)len};
+				pthread_mutex_unlock(&cachelock);
+				fi->fh = fd;
+				return fd;
+			}
 		} else {
 			return -errno;
 		}
@@ -305,12 +215,28 @@ static int xpfs_read(const char *path, char *buf, size_t size, off_t offset,
 	int res;
 
 	if (fi == NULL) {
-		char rwpath[strlen(basepath)+strlen(path)+2];
-		sprintf(rwpath, "%s/%s", basepath, path);
+		char rwpath[strlen(config.basepath)+strlen(path)+2];
+		sprintf(rwpath, "%s/%s", config.basepath, path);
 		fd = open(path, O_RDONLY);
 	} else fd = fi->fh;
-	
 	if (fd == -1) return -errno;
+	
+	if (!config.cache) {
+		pthread_mutex_lock(&cachelock);
+		if (DEBUG >= 2) printf("read: Uncached mode, looking for FD %d.\n", fd);
+		auto i = memcache.find(fd);
+		if (i != memcache.end()) {
+			if (DEBUG >= 2) printf("read: %d bytes at %d from memory for FD %d.\n", size, offset, i->first);
+			if (size+offset > i->second.len) {
+				size = (i->second.len)-offset;
+				if (DEBUG) printf("read: Read would have exceeded length, reducing to %d.\n", size);
+			}
+			memcpy(buf, (i->second.data)+offset, size);
+			pthread_mutex_unlock(&cachelock);
+			return size;
+		}
+		pthread_mutex_unlock(&cachelock);
+	}
 
 	res = pread(fd, buf, size, offset);
 	if (res == -1) res = -errno;
@@ -323,12 +249,31 @@ static int xpfs_read(const char *path, char *buf, size_t size, off_t offset,
 static int xpfs_release(const char *path, struct fuse_file_info *fi)
 {
 	if (DEBUG >= 2) printf("release: %s\n", path);
-	if (fi != NULL) return close(fi->fh);
-	return 0;
+	if (fi == NULL || fi->fh == 0) return 0;
+	if (!config.cache) {
+		pthread_mutex_lock(&cachelock);
+		auto i = memcache.find(fi->fh);
+		if (i != memcache.end()) {
+			if (DEBUG >= 2) printf("release: Freeing %d bytes of memory for FD %d.\n", i->second.len, i->first);
+			free(i->second.data);
+			memcache.erase(i);
+			pthread_mutex_unlock(&cachelock);
+			return 0;
+		}
+		pthread_mutex_unlock(&cachelock);
+	}
+	return close(fi->fh);
 }
 
 int main(int argc, char *argv[])
 {
+	umask(0);
+	config.cache = 1;
+	config.compress = 1;
+
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+	fuse_opt_parse(&args, &config, xpfs_opts, xpfs_opt_proc);
+
 	struct fuse_operations oper;
 	memset(&oper, 0, sizeof(oper));
 	oper.getattr = xpfs_getattr;
@@ -337,16 +282,12 @@ int main(int argc, char *argv[])
 	oper.read = xpfs_read;
 	oper.release = xpfs_release;
 	
-	umask(0);
-	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-	fuse_opt_parse(&args, NULL, NULL, xpfs_opt_proc);
-	
-	if (basepath == NULL) {
+	if (config.basepath == NULL) {
 		fprintf(stderr, "Usage: %s <scenery> <mount>\n", args.argv[0]);
 		return 1;
 	}
 	
-	printf("Starting: basepath=%s\n", basepath);
+	printf("Starting: basepath=%s cache=%d format=%s\n", config.basepath, config.cache, config.compress?"DXT1":"RGB");
 	int ret = fuse_main(args.argc, args.argv, &oper, NULL);
 	printf("Exiting.\n");
 	
